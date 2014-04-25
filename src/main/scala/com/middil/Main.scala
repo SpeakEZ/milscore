cfd4f595ac3af99575cd72cb92fd41ca5b72a887
Eric Smith <esmith@Erics-MacBook-Air-2.local>
2014-04-25 16:19:25 -0400

    SPIK-161 - Add future for Akka response; Add gitignore exclusion for IntelliJ and Emacs specific files

--- src/main/scala/com/middil/Main.scala
+++ src/main/scala/com/middil/Main.scala
@@ -1,10 +1,17 @@
 package com.middil
 
 import akka.actor.ActorSystem
+import akka.io.IO
+import spray.can.Http
+import spray.http.HttpRequest
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
 
 object Main extends App {
+  implicit val system = ActorSystem()
 
-  val system = ActorSystem()
+  IO(Http) ! HttpRequest(uri = "http://scala.io")
 
   system.shutdown()
 
