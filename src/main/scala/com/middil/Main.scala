ad92f385bc0dad1d2d55e6fb6975e8b510eab939
Eric Smith <esmith@Erics-MacBook-Air-2.local>
2014-04-25 16:27:05 -0400

    SPIK-161 - Add missing import and timeout for future

--- src/main/scala/com/middil/Main.scala
+++ src/main/scala/com/middil/Main.scala
@@ -3,16 +3,21 @@ package com.middil
 import akka.actor.ActorSystem
 import akka.io.IO
 import spray.can.Http
-import spray.http.HttpRequest
+import spray.http.{HttpResponse, HttpRequest}
 import akka.pattern.ask
 import akka.util.Timeout
 import scala.concurrent.duration._
 
 object Main extends App {
+  implicit val timeout: Timeout = 2.seconds
   implicit val system = ActorSystem()
+  import system.dispatcher
 
-  IO(Http) ! HttpRequest(uri = "http://scala.io")
+  val future = IO(Http) ? HttpRequest(uri = "http://spray.io")
 
-  system.shutdown()
+  future.mapTo[HttpResponse] onComplete {response =>
+    println(s"The response is $response")
+    system.shutdown()
+  }
 
 }
