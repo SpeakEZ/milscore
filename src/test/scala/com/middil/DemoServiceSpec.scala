860446d4f17057aec0da139588573667a522070a
Eric Smith <esmith@Erics-MacBook-Air-2.local>
2014-05-06 20:18:47 -0400

    SPIK-161 - Add test for color route

--- src/test/scala/com/middil/DemoServiceSpec.scala
+++ src/test/scala/com/middil/DemoServiceSpec.scala
@@ -4,6 +4,12 @@ import org.scalatest._
 import spray.testkit.ScalatestRouteTest
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers
+import spray.httpx.encoding.Gzip
+import spray.httpx.unmarshalling._
+import spray.http._
+import HttpHeaders._
+import HttpEncodings._
+
 
 class DemoServiceSpec extends FreeSpec with ScalatestRouteTest with Matchers with DemoService {
   def actorRefFactory = system
@@ -16,6 +22,18 @@ class DemoServiceSpec extends FreeSpec with ScalatestRouteTest with Matchers wit
 	}
       }
     }
+
+    "when calling GET  /color?red=64&green=192&blue=164" - {
+      "should return hex value #40c0a4" in {
+	Get("/color?red=64&green=192&blue=164") ~> route ~> check {
+	  header[`Content-Encoding`] === Some(`Content-Encoding`(gzip))
+	  Gzip.decode(response).entity.as[String].right.get should include("#40c0a4")
+	}
+      }
+    }
+
   }
+
+
 
 }
